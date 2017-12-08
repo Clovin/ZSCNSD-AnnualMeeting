4f726119220b8c9af5e360bd1939ca2d3e5aee2c
Clovin <wxxzhuang@gmail.com>
2017-12-08 11:39:49 +0800

    Fix a CSS error

--- config/index.js
+++ config/index.js
@@ -9,8 +9,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    // assetsPublicPath: './',
+    // assetsPublicPath: '/',
+    assetsPublicPath: './',
     productionSourceMap: false, // 改为false
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
